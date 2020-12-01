d60452ebb94f5f9daeafb3048ffe75830fe2ecaf
Rishabh Ranjan <rishabhranjan461@gmail.com>
2020-12-01 15:11:50 -0500

    clean up day 1 code

--- report_repair.py
+++ report_repair.py
@@ -5,27 +5,28 @@ Author:         Rishabh Ranjan
 Last Modified:  12/1/2020
 '''
 
+def two_entries_with_sum(expense_report, sum):
+    for i in range(len(expense_report)):
+        for j in range(i + 1, len(expense_report)):
+            if expense_report[i] + expense_report[j] == sum:
+                return expense_report[i]*expense_report[j]
+
+def three_entries_with_sum(expense_report, sum):
+    for i in range(len(expense_report)):
+        for j in range(i + 1, len(expense_report)):
+            for k in range(j + 1, len(expense_report)):
+                if expense_report[i] + expense_report[j] + expense_report[k] == sum:
+                    return expense_report[i]*expense_report[j]*expense_report[k]
+
 def main():
     f = open('day_1_input.txt', 'r')
     expense_report = []
     for x in f:
-        expense_report.append(x[:len(x) - 1])
+        expense_report.append(int(x))
     f.close()
-    found = False
-    for i in range(len(expense_report)):
-        for j in range(i + 1, len(expense_report)):
-            if int(expense_report[i]) + int(expense_report[j]) == 2020:
-                print("Part 1 Answer: ", int(expense_report[i])*int(expense_report[j]))
-                found = True
-                break
-        if found:
-            break
-    for i in range(len(expense_report)):
-        for j in range(i + 1, len(expense_report)):
-            for k in range(j + 1, len(expense_report)):
-                if int(expense_report[i]) + int(expense_report[j]) + int(expense_report[k]) == 2020:
-                    print("Part 2 Answer: ", int(expense_report[i])*int(expense_report[j])*int(expense_report[k]))
-                    return
+    sum = 2020
+    print("Part 1 Answer: ", two_entries_with_sum(expense_report, sum))
+    print("Part 2 Answer: ", three_entries_with_sum(expense_report, sum))
 
 if __name__ == '__main__':
     main()
