4046bff144c5722bcbbda175ea9996a2cab5276b
Rishabh Ranjan <rishabhranjan461@gmail.com>
2020-12-03 01:10:15 -0500

    finish day 3

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Puzzles can be found at https://adventofcode.com/2020
 Day 1: Report Repair
 
 Day 2: Password Philosophy
+
+Day 3: Toboggan Trajectory
---
+++ toboggan_trajectory.py
@@ -0,0 +1,27 @@
+'''
+Day:            3
+File:           toboggan_trajectory.py
+Author:         Rishabh Ranjan
+Last Modified:  12/3/2020
+'''
+
+def count_trees(grid, right, down):
+    num_trees = 0
+    curr_pos = 0
+    for i in range(0, len(grid), down):
+        if grid[i][curr_pos] == '#':
+            num_trees += 1
+        curr_pos = (curr_pos + right) % (len(grid[0]) - 1)
+    return num_trees
+
+def main():
+    f = open('day_3_input.txt', 'r')
+    grid = []
+    for x in f:
+        grid.append(x)
+    f.close()
+    print("Part 1 Answer: ", count_trees(grid, 3, 1))
+    print("Part 2 Answer: ", count_trees(grid, 1, 1)*count_trees(grid, 3, 1)*count_trees(grid, 5, 1)*count_trees(grid, 7, 1)*count_trees(grid, 1, 2))
+
+if __name__ == '__main__':
+    main()
